24b74898dfead638239ec01821e64f7b2583ab2e
thegreatjy <thegreatjy@gmail.com>
2023-11-27 01:38:01 +0900

    feat: 로그인 필터 추가

--- src/main/java/com/gck/filter/LoginCheckFilter.java
+++ src/main/java/com/gck/filter/LoginCheckFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpSession;
 // 글 작성하기, 글 수정하기, 글 삭제하기
 // 댓글 작성하기, 댓글 수정하기, 댓글 삭제하기
 // 전에 필터를 통해 로그인 정보를 확인한다.
-@WebFilter(filterName = "LoginFilter", urlPatterns = {""})
+@WebFilter(filterName = "LoginFilter", urlPatterns = {"/member/login.jsp"})
 public class LoginCheckFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -25,15 +25,17 @@ public class LoginCheckFilter implements Filter {
             HttpSession session = httpRequest.getSession(false);
 
             // 로그인된 세션 정보가 없다 || 세션의 memberId가 없다
-            if (session == null || session.getAttribute("memberId") == null) {
+            if (session == null || session.getAttribute("memberIdx") == null) {
                 System.out.println("미인증 사용자 요청 {}"+ requestURI);
                 // 로그인으로 redirect
                 // 로그인 컨트롤러에서 redirectURL이 있으면 로그인 성공 후 redirectURL로 이동하게 된다.
-                httpResponse.sendRedirect("member/login?redirectURL="+ requestURI);
+                httpResponse.sendRedirect("../member/login.do?redirectURL="+ requestURI);
+                System.out.println("request uri" + requestURI);
                 return;
             }
 
             // controller로 넘어간다.
+            System.out.println("durl ");
             chain.doFilter(request, response);
         } catch (Exception e) {
             throw e;
