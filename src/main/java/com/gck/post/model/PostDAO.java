8efd02ad894db55335aed97d7c4cea34db9e8ebd
권진철 <sunkiro96@naver.com>
2023-11-28 03:18:14 +0900

    feat: Post dao 추가

--- src/main/java/com/gck/post/model/PostDAO.java
+++ src/main/java/com/gck/post/model/PostDAO.java
@@ -8,4 +8,9 @@ public interface PostDAO {
     // 검색 조건에 맞는 게시물 목록을 반환 (페이징)
     public List<BoardVO> selectListPage();
     public List<BoardVO> selectListMyPage();
+    public PostVO selectView(String postIdx);
+    public int updateVisitCount(String postIdx);
+    public int insertPost(PostVO vo);
+    public int updatePost(PostVO vo);
+    public int deletePost(int postIdx);
 }
