7c2d597301886a3921fc779b4881d472f40bd359
권진철 <sunkiro96@naver.com>
2023-11-28 14:52:32 +0900

    feat: POSTDAO 인터페이스를 받아줄 impl 파일 추가

---
+++ src/main/java/com/gck/post/model/PostDAOImpl.java
@@ -0,0 +1,90 @@
+package com.gck.post.model;
+
+import com.gck.factory.MyBatisFactory;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+import java.util.Map;
+
+public class PostDAOImpl implements PostDAO {
+
+    public List<PostVO> selectListPage(Map<String, Object> map) {
+        SqlSession sqlSession = MyBatisFactory.getSqlSession();
+        PostDAO mapper = sqlSession.getMapper(PostDAO.class);
+        List<PostVO> result = mapper.selectListPage(map);
+        sqlSession.close();
+        return result;
+    }
+
+    public int selectCount(Map<String, Object> map) {
+        SqlSession sqlSession = MyBatisFactory.getSqlSession();
+        PostDAO mapper = sqlSession.getMapper(PostDAO.class);
+        int result = mapper.selectCount(map);
+        System.out.println("selectCount - 행 개수 = " + result);
+        sqlSession.close();
+        return result;
+    }
+
+    public int insertPost(PostVO vo) {
+        SqlSession sqlSession = MyBatisFactory.getSqlSession();
+        PostDAO mapper = sqlSession.getMapper(PostDAO.class);
+        int result = mapper.insertPost(vo);
+        if (result == 1) {
+            sqlSession.commit();
+            System.out.println("새로운 게시글 저장 성공");
+        } else {
+            System.out.println("새로운 게시글 저장 실패");
+        }
+        sqlSession.close();
+        return result;
+    }
+
+    public PostVO selectView(String noticeIdx) {
+        SqlSession sqlSession = MyBatisFactory.getSqlSession();
+        PostDAO mapper = sqlSession.getMapper(PostDAO.class);
+        PostVO vo = mapper.selectView(noticeIdx);
+
+        sqlSession.close();
+        return vo;
+    }
+
+    public int updateVisitCount(String postIdx) {
+        SqlSession sqlSession = MyBatisFactory.getSqlSession();
+        PostDAO mapper = sqlSession.getMapper(PostDAO.class);
+        int result = mapper.updateVisitCount(postIdx);
+        System.out.println("update query result val = " + result);
+        if (result == 1) {
+            sqlSession.commit();
+        } else {
+            System.out.println("조회수 증가 중 오류 발생");
+        }
+        sqlSession.close();
+        return result;
+    }
+
+    public int deletePost(int postIdx) {
+        SqlSession sqlSession = MyBatisFactory.getSqlSession();
+        PostDAO mapper = sqlSession.getMapper(PostDAO.class);
+        int result = mapper.deletePost(postIdx);
+        if (result == 1) {
+            sqlSession.commit();
+        } else {
+            System.out.println("게시판 글 삭제 중 오류 발생...");
+        }
+
+        return result;
+    }
+
+    public int updatePost(PostVO vo) {
+        SqlSession sqlSession = MyBatisFactory.getSqlSession();
+        PostDAO mapper = sqlSession.getMapper(PostDAO.class);
+        int result = mapper.updatePost(vo);
+        if (result == 1) {
+            sqlSession.commit();
+        } else {
+            System.out.println("게시판 글 update 중 오류 발생...");
+        }
+        sqlSession.commit();
+        return result;
+    }
+}
