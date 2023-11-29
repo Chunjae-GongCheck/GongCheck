9ccc11dbf23495de7f468b9c9de16cda6ec9b037
thegreatjy <thegreatjy@gmail.com>
2023-11-30 01:09:59 +0900

    feat: 회원가입 시, 회원 비밀번호 insert

--- src/main/java/com/gck/member/model/PasswordMemberDAO.java
+++ src/main/java/com/gck/member/model/PasswordMemberDAO.java
@@ -2,5 +2,5 @@ package com.gck.member.model;
 
 public interface PasswordMemberDAO {
     // 회원가입
-    public Integer insertPassword(String memberId, String encryptedPasswordMember);
+    public Integer insertPassword(PasswordMemberVO passwordMemberVO);
 }
