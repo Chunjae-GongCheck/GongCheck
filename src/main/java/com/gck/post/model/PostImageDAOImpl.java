d53f91afe17e7fad4ce5195bf411dbcc9aed3312
권진철 <sunkiro96@naver.com>
2023-11-29 23:17:09 +0900

    feat: 수정, 삭제 부분 DAO Impl 추가

--- src/main/java/com/gck/post/model/PostImageDAOImpl.java
+++ src/main/java/com/gck/post/model/PostImageDAOImpl.java
@@ -25,4 +25,34 @@ public class PostImageDAOImpl implements PostImageDAO {
         sqlSession.close();
         return result;
     }
+
+    @Override
+    public int updatePostImage(PostImageVO vo) {
+        SqlSession sqlSession = MyBatisFactory.getSqlSession();
+        PostImageDAO mapper = sqlSession.getMapper(PostImageDAO.class);
+        int result = mapper.updatePostImage(vo);
+        if (result == 1) {
+            System.out.println("PostImage가 성공적으로 수정되었습니다.");
+            sqlSession.commit();
+        } else {
+            System.out.println("PostImage 수정 실패.");
+        }
+        sqlSession.close();
+        return result;
+    }
+
+    @Override
+    public int deletePostImage(PostImageVO vo) {
+        SqlSession sqlSession = MyBatisFactory.getSqlSession();
+        PostImageDAO mapper = sqlSession.getMapper(PostImageDAO.class);
+        int result = mapper.deletePostImage(vo);
+        if (result == 1) {
+            System.out.println("PostImage가 성공적으로 삭제되었습니다.");
+            sqlSession.commit();
+        } else {
+            System.out.println("PostImage 삭제 실패.");
+        }
+        sqlSession.close();
+        return result;
+    }
 }
