dd3d72ff35a250f7eadfd8de2fd2fa240dd50b6e
jihye <jnelnl@naver.com>
2023-11-30 19:57:08 +0900

    docs: 댓글 불러오기 수정

--- src/main/java/com/gck/reply/controller/ReplyController.java
+++ src/main/java/com/gck/reply/controller/ReplyController.java
@@ -1,13 +1,25 @@
 package com.gck.reply.controller;
 
+import com.gck.reply.model.ReplyVO;
 import com.gck.reply.service.ReplyService;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
 
-@WebServlet("/replies")
+@WebServlet(name="ReplyView", value="/gck/PostViews.do")
 public class ReplyController extends HttpServlet {
     private static final long serialVersionUID = 1L;
+
+    public ReplyController(){
+        super();
+        System.out.println("ReplyController_test");
+    }
 
     private static class ReplyServiceHelper{
         private static final ReplyService ReplyService = new ReplyService();
@@ -16,10 +28,23 @@ public class ReplyController extends HttpServlet {
     public static ReplyService getRepliesByPost(){
         return ReplyServiceHelper.ReplyService;
     }
-    //   @GetMapping("/replies/list/{postIdx}")
-    //  public String getRepliesByPost(@PathVariable("postIdx") int postIdx, Model model) {
-    //      List<ReplyVO> replies = replyService.getRepliesByPost(postIdx);
-    //     model.addAttribute("replies", replies);
-    //     return "Reply";
-    //  }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("ReplyController_test");
+        ReplyService replyService = new ReplyService();
+        System.out.println(req.getParameter("postIdx"));
+        List<ReplyVO> list = replyService.getRepliesByPost(Integer.parseInt(req.getParameter("postIdx")));
+        //System.out.println((Integer) req.getAttribute("postIdx"));
+
+        //테스트용
+        for(ReplyVO replyVO: list){
+            System.out.println(replyVO.getReplyContent());
+        }
+
+        //뷰 전달
+        req.setAttribute("list",list);
+        req.getRequestDispatcher("../reply/Reply.jsp").forward(req,resp);
+    }
+
 }
