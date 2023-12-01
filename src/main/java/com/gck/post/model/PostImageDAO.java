284ebbf6e107272ab4637f9b3d5b005b95b80b06
권진철 <sunkiro96@naver.com>
2023-12-01 12:23:24 +0900

    docs: 파일 삭제를 위한 PostImageDAO 수정(맨 뒷부분만 추가했습니다 추후 수정 예정)

--- src/main/java/com/gck/post/model/PostImageDAO.java
+++ src/main/java/com/gck/post/model/PostImageDAO.java
@@ -1,5 +1,6 @@
 package com.gck.post.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +15,7 @@ public interface PostImageDAO {
 
     int deletePostImage(PostImageVO vo);
 
+    ArrayList<PostImageVO> getPostImagesByPostIdx(int postIdx);
 
 }
 
