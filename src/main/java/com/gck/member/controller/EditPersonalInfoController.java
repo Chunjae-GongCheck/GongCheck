75b4c64858359ea4da06439e92f47e01ea021920
thegreatjy <thegreatjy@gmail.com>
2023-12-03 21:18:11 +0900

    feat: 회원정보 수정 controller servlet 추가

--- src/main/java/com/gck/member/controller/EditPersonalInfoController.java
+++ src/main/java/com/gck/member/controller/EditPersonalInfoController.java
@@ -1,6 +1,5 @@
 package com.gck.member.controller;
 
-import com.gck.member.model.PasswordMemberVO;
 import com.gck.member.service.MemberService;
 import com.gck.member.model.MemberVO;
 
@@ -13,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 // 회원 정보 수정을 수행하는 컨트롤러
 @WebServlet("/member/edit.do")
@@ -42,7 +42,7 @@ public class EditPersonalInfoController extends HttpServlet {
         String url = req.getContextPath();
 
         // 로그인 되어 있는 회원 idx
-        String memberIdx = (String)session.getAttribute("memberIdx");
+        Object memberIdx = session.getAttribute("memberIdx");
 
         // 세션에 로그인 정보가 없을 경우 메인 화면으로 redirect
         if(memberIdx == null){
@@ -52,7 +52,7 @@ public class EditPersonalInfoController extends HttpServlet {
         }
 
         // 회원 정보 가져옴
-        MemberVO memberVo = getMemberService().getMember(memberIdx);
+        MemberVO memberVo = getMemberService().getMember(memberIdx.toString());
 
         // 실패
         if(memberVo == null){
@@ -63,7 +63,7 @@ public class EditPersonalInfoController extends HttpServlet {
             resp.setContentType("text/html;charset=UTF-8");
             PrintWriter writer = resp.getWriter();
             String script = "<script>"
-                    + "    alert('" + "회원 정보 수정을 하실 수 없습니다." + "');"
+                    + "    alert('" + "회원 정보 수정을 하실 수 없습니다. 회원 조회가 불가능합니다." + "');"
                     + "    location.href='" + url + "';"
                     + "</script>";
             writer.print(script);
@@ -73,14 +73,14 @@ public class EditPersonalInfoController extends HttpServlet {
         }
 
         // 회원 정보를 페이지에 출력
-        req.setAttribute("memberVO", memberVo);
+        req.setAttribute("memberVo", memberVo);
 
-        url += "/member/edit.jsp";
+        url = "/member/edit.jsp";
         RequestDispatcher dispatcher = req.getRequestDispatcher(url);
         dispatcher.forward(req, resp);
     }
 
-    // 회원 정보 수정 기능 수행
+    // 회원정보 수정 기능 수행
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 세션 객체 생성
@@ -89,13 +89,51 @@ public class EditPersonalInfoController extends HttpServlet {
         // 이동할 url
         String url = req.getContextPath();
 
+        // 로그인 되어 있는 회원 idx
+        Object memberIdx = session.getAttribute("memberIdx");
+
         // 세션에 로그인 정보가 없을 경우 메인 화면으로 redirect
-        if(session.getAttribute("memberIdx") == null){
+        if(memberIdx == null){
             url += "/index.jsp";  // [@@@@@@@@@@] 메인 페이지로 수정해야 함. 메인 페이지로 가는 컨트롤러(board/main.do)로 연결해야 한다.
             resp.sendRedirect(url);
             return;
         }
 
+        // 회원정보 수정 폼에서 정보를 받아와 hashmap에 넣는다.
+        HashMap<String, String> map = new HashMap<>();
+        map.put("memberIdx", memberIdx.toString());
+        map.put("memberNickname", req.getParameter("memberNickname"));
+        map.put("memberEmail", req.getParameter("memberEmail"));
+        map.put("memberZonecode", req.getParameter("memberZonecode"));
+        map.put("memberAddress", req.getParameter("memberAddress"));
+        map.put("memberAddressDetailed", req.getParameter("memberAddressDetailed"));
 
+
+        // 회원정보 수정
+        boolean result = getMemberService().updateMember(map);
+
+        url += "/member/edit.do";
+        // 회원정보 수정 성공 여부에 따른 메세지
+        String msg = "";
+
+        if(result){ // 성공
+            msg = "회원정보를 수정하였습니다.";
+            // 세션에 변경된 닉네임 update
+            session.removeAttribute("memberNickname");
+            session.setAttribute("memberNickname", req.getParameter("memberNickname"));
+        }else{      // 실패
+            msg = "회원정보 수정에 실패하였습니다.";
+        }
+
+        // alert 출력
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+        String script = "<script>"
+                + "    alert('" + msg + "');"
+                + "    location.href='" + url + "';"
+                + "</script>";
+        writer.print(script);
+        writer.flush();
+        writer.close();
     }
 }
