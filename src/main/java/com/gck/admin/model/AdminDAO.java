8caf2af899dada244537cbef06fdf1f19f79d48f
seokeunparks <pppse620@gmail.com>
2023-12-05 15:10:25 +0900

    feat/관리자 회원정보 수정

--- src/main/java/com/gck/admin/model/AdminDAO.java
+++ src/main/java/com/gck/admin/model/AdminDAO.java
@@ -1,5 +1,11 @@
 package com.gck.admin.model;
 
+import java.util.List;
+
 public interface AdminDAO {
 
+    List<AdminVO> selectAdminList();
+    int deleteAdmin(String adminId);
+
+    AdminVO selectAdmin(String adminId);
 }
