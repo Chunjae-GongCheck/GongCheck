b6a1334a9fcbf0beedf8bff31d79fb0858462202
seokeunparks <pppse620@gmail.com>
2023-12-07 17:37:03 +0900

    style : 주석 수정

--- src/main/java/com/gck/admin/model/AdminResultVO.java
+++ src/main/java/com/gck/admin/model/AdminResultVO.java
@@ -1,19 +1,25 @@
-//package com.gck.admin.model;
-//
-//import lombok.Data;
-//
-//@Data
-//public class AdminResultVO {
-//    private int adminIdx;       // 관리자 index
-//    private String adminId;     // 관리자 id
-//    private int passwordAdminIdx;   // 비밀번호 index
-//    private String passwordAdmin;   // 비밀번호
-//
-//    public AdminResultVO(AdminVO adminVo, PasswordAdminVO passwordAdminVo) {
-//        this.adminIdx = adminVo.getAdminIdx();
-//        this.adminId = adminVo.getAdminId();
-//        this.passwordAdminIdx = passwordAdminVo.getPasswordAdminIdx();
-//        this.passwordAdmin = passwordAdminVo.getPasswordAdmin();
-//    }
-//
-//}
+package com.gck.admin.model;
+
+import lombok.Data;
+
+
+// ** 회원정보를 처리(조회, 결과)하기 위한 관리자 VO 클래스입니다. **//
+
+@Data
+public class AdminResultVO {
+    private int adminIdx;       // 관리자 index
+    private String adminId;     // 관리자 id
+    private int passwordAdminIdx;   // 비밀번호 index
+    private String passwordAdmin;   // 비밀번호
+
+    // AdminVO와 PasswordAdminVO를 받아서 AdminResultVO를 생성하는 생성자입니다.
+    // adminVO : 관리자 정보
+    // passwordAdminVO : 비밀번호 정보
+    public AdminResultVO(AdminVO adminVo, PasswordAdminVO passwordAdminVo) {
+        this.adminIdx = adminVo.getAdminIdx();
+        this.adminId = adminVo.getAdminId();
+        this.passwordAdminIdx = passwordAdminVo.getPasswordAdminIdx();
+        this.passwordAdmin = passwordAdminVo.getPasswordAdmin();
+    }
+
+}
