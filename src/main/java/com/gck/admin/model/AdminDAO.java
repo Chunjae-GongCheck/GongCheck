1ba481c4ff86f1739ffa01adf1ccb9250a5c2f04
seokeunparks <pppse620@gmail.com>
2023-12-07 17:36:57 +0900

    style : 주석 수정

--- src/main/java/com/gck/admin/model/AdminDAO.java
+++ src/main/java/com/gck/admin/model/AdminDAO.java
@@ -1,11 +1,21 @@
-//package com.gck.admin.model;
-//
-//import java.util.List;
-//
-//public interface AdminDAO {
-//
-//    List<AdminVO> selectAdminList();
-//    int deleteAdmin(String adminId);
-//
-//    AdminVO selectAdmin(String adminId);
-//}
+package com.gck.admin.model;
+
+import java.util.List;
+
+// ** 회원정보를 처리하기 위한 관리자 DAO 인터페이스입니다. **//
+
+public interface AdminDAO {
+
+
+    // 회원정보를 조회하는 메소드입니다.
+    // 조회 결과는 AdminResultVO 객체에 저장되어 반환됩니다.
+    List<AdminVO> selectAdminList();
+
+    // 회원정보를 삭제하는 메소드입니다.
+    // 삭제할 회원의 idx를 파라미터로 전달받습니다.
+    int deleteAdmin(String adminId);
+
+    // 회원정보를 수정하는 메소드입니다.
+    // 수정할 회원의 idx와 새로운 비밀번호를 파라미터로 전달받습니다.
+    AdminVO selectAdmin(String adminId);
+}
