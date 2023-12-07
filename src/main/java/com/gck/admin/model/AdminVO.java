e7becf5e9e76caae36b062803e1123af74551bef
seokeunparks <pppse620@gmail.com>
2023-12-07 17:37:08 +0900

    style : 주석 수정

--- src/main/java/com/gck/admin/model/AdminVO.java
+++ src/main/java/com/gck/admin/model/AdminVO.java
@@ -1,17 +1,13 @@
-//package com.gck.admin.model;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import lombok.ToString;
-//
-//// 관리자
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@ToString
-//
-//public class AdminVO {
-//    private int adminIdx;       // 관리자 index
-//    private String adminId;     // 관리자 id
-//}
+package com.gck.admin.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+//** 관리자 정보를 담는 VO 클래스 입니다. **//
+
+@Data
+@AllArgsConstructor
+public class AdminVO {
+    private int adminIdx;       // 관리자 index
+    private String adminId;     // 관리자 id
+}
